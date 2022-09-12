48ceea2081f705cd44e44188160e3a2a28d95606
chandram13 <mschandr@uci.edu>
2022-09-12 12:36:31 -0700

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+// Companion Objects
+
+import scala.math
+
+class Trapezoid():
+  def trapezoidArea(h,b1,b2):
+    1/2 * h * (b1 + b2)
+object TrapezoidAddition:
+  public def calculatePerimeter(T,B,L,R):
+    T + B + L + R
